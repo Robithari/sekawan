0d37aae9975fe5004dddc6aa7c404888241925a3
Robithari <141445037+Robithari@users.noreply.github.com>
2024-07-09 02:14:40 +0700

    update

--- js/kunci-layar.js
+++ js/kunci-layar.js
@@ -1,12 +1,12 @@
 // Mendeteksi perubahan orientasi layar
 window.addEventListener('orientationchange', function() {
-    // Jika orientasi layar adalah landscape, kunci ke mode portrait
-    if (screen.orientation.angle !== 0) {
-        screen.orientation.lock('portrait');
+    // Jika orientasi layar adalah landscape, kembalikan ke mode portrait
+    if (window.orientation === 90 || window.orientation === -90) {
+        // Mengatur kembali orientasi ke portrait
+        setTimeout(function() {
+            if (window.orientation !== 0) {
+                window.orientation = 0;
+            }
+        }, 0);
     }
 });
-
-// Kunci orientasi saat halaman dimuat pertama kali
-if (screen.orientation.angle !== 0) {
-    screen.orientation.lock('portrait');
-}
