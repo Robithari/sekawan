077b22970c0af73cd90c3971ad9cc3e8776e56e3
Robithari <141445037+Robithari@users.noreply.github.com>
2024-07-10 23:48:50 +0700

    update

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,44 @@
 // service-worker.js
 
+// Event 'install'
 self.addEventListener('install', function(event) {
-    event.waitUntil(
-      caches.open('cache-v1').then(function(cache) {
-        return cache.addAll([
-          '/',
-          // Daftar file lain yang perlu disimpan dalam cache
-        ]);
-      })
-    );
-  });
+  event.waitUntil(
+    caches.open('cache-v1').then(function(cache) {
+      return cache.addAll([
+        '/',
+        // Daftar file lain yang perlu disimpan dalam cache
+        // Misalnya: '/index.html', '/styles.css', '/script.js'
+      ]);
+    }).catch(function(error) {
+      console.error('Failed to open cache:', error);
+    })
+  );
+});
+
+// Event 'activate'
+self.addEventListener('activate', function(event) {
+  var cacheWhitelist = ['cache-v1'];
   
-  self.addEventListener('fetch', function(event) {
-    event.respondWith(
-      caches.match(event.request).then(function(response) {
-        return response || fetch(event.request);
-      })
-    );
-  });
-  
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Event 'fetch'
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    }).catch(function(error) {
+      console.error('Failed to fetch:', error);
+    })
+  );
+});
