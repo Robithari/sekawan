4c9dfe6f5b22941d1a2b95cbbab849370b95311a
Robithari <141445037+Robithari@users.noreply.github.com>
2024-08-16 23:06:07 +0700

    update

--- service-worker.js
+++ service-worker.js
@@ -1,31 +1,26 @@
-const CACHE_NAME = 'cache-v3'; // Versi cache, sesuaikan jika diperlukan
-
 self.addEventListener('install', function(event) {
   // Tidak ada file yang dicache selama proses instalasi
   self.skipWaiting(); // Memaksa service worker baru untuk segera aktif
 });
 
 self.addEventListener('activate', function(event) {
-  var cacheWhitelist = [CACHE_NAME];
-
+  // Menghapus semua cache yang ada
   event.waitUntil(
     caches.keys().then(function(cacheNames) {
       return Promise.all(
         cacheNames.map(function(cacheName) {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
+          return caches.delete(cacheName);
         })
       );
     }).catch(function(error) {
-      console.error('Failed to delete old caches:', error);
+      console.error('Failed to delete caches:', error);
     })
   );
   self.clients.claim(); // Memastikan semua tab yang terbuka menggunakan SW yang baru
 });
 
 self.addEventListener('fetch', function(event) {
-  // Always fetch the latest version from the network for all files
+  // Always fetch the latest version from the network for all requests
   event.respondWith(
     fetch(event.request)
       .then(function(networkResponse) {
