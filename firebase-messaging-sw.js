a37d51fb37289bee019ad25fdf36c5f216024823
Robithari <141445037+Robithari@users.noreply.github.com>
2024-08-19 23:20:05 +0700

    update

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,31 +1,30 @@
-// firebase-messaging-sw.js
-
 importScripts('https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/9.6.1/firebase-messaging.js');
 
-// Inisialisasi Firebase di Service Worker
-const firebaseConfig = {
-  apiKey: "AIzaSyDo2kyDl39c4t5DfxYycmmjHSbY5FXB9AA",
-  authDomain: "sekawan-fc-427414.firebaseapp.com",
-  projectId: "sekawan-fc-427414",
-  storageBucket: "sekawan-fc-427414.appspot.com",
-  messagingSenderId: "399174955835",
-  appId: "1:399174955835:web:c681f8681c474420e8fd1e",
-  measurementId: "G-CD0MHD1RBP"
-};
+// Periksa apakah firebase dan messaging sudah dimuat
+if (firebase && firebase.messaging) {
+  firebase.initializeApp({
+    apiKey: "AIzaSyDo2kyDl39c4t5DfxYycmmjHSbY5FXB9AA",
+    authDomain: "sekawan-fc-427414.firebaseapp.com",
+    projectId: "sekawan-fc-427414",
+    storageBucket: "sekawan-fc-427414.appspot.com",
+    messagingSenderId: "399174955835",
+    appId: "1:399174955835:web:c681f8681c474420e8fd1e",
+    measurementId: "G-CD0MHD1RBP"
+  });
 
-firebase.initializeApp(firebaseConfig);
-const messaging = firebase.messaging();
+  const messaging = firebase.messaging();
 
-// Menangani pesan latar belakang
-messaging.onBackgroundMessage((payload) => {
-  console.log('Pesan latar belakang diterima: ', payload);
-  // Kustomisasi notifikasi di sini
-  const notificationTitle = 'Background Message Title';
-  const notificationOptions = {
-    body: 'Background Message body.',
-    icon: '/firebase-logo.png'
-  };
+  messaging.onBackgroundMessage(function(payload) {
+    console.log('[firebase-messaging-sw.js] Received background message ', payload);
+    const notificationTitle = payload.notification.title;
+    const notificationOptions = {
+      body: payload.notification.body,
+      icon: '/firebase-logo.png'
+    };
 
-  self.registration.showNotification(notificationTitle, notificationOptions);
-});
+    return self.registration.showNotification(notificationTitle, notificationOptions);
+  });
+} else {
+  console.error('Firebase atau Firebase Messaging tidak tersedia.');
+}
