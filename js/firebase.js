eecc4fb8d35f65d62696f9eabc42988ed057ae86
Robithari <141445037+Robithari@users.noreply.github.com>
2024-08-20 20:51:43 +0700

    update

--- js/firebase.js
+++ js/firebase.js
@@ -1,6 +1,8 @@
+// Mengimpor modul yang dibutuhkan dari Firebase SDK
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-analytics.js";
 
+// Konfigurasi Firebase untuk aplikasi Anda
 const firebaseConfig = {
   apiKey: "AIzaSyDo2kyDl39c4t5DfxYycmmjHSbY5FXB9AA",
   authDomain: "sekawan-fc-427414.firebaseapp.com",
@@ -11,5 +13,8 @@ const firebaseConfig = {
   measurementId: "G-CD0MHD1RBP"
 };
 
+// Inisialisasi Firebase
 const app = initializeApp(firebaseConfig);
+
+// Inisialisasi Google Analytics
 const analytics = getAnalytics(app);
