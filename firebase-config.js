9ee52fc480462bf77be3c2f004b2ad9cf350531f
Robithari <141445037+Robithari@users.noreply.github.com>
2024-08-26 13:27:02 +0700

    update

--- firebase-config.js
+++ firebase-config.js
@@ -1,16 +1,49 @@
-// Mengatur Service Worker dari versi Firebase 8
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/firebase-messaging-sw.js')
-    .then((registration) => {
-      console.log('Service Worker registered with scope:', registration.scope);
-      return getToken(messaging, { vapidKey: 'BPAqQpY9sfUZKGfJVpq6HKFoQp4THJ-ESMjE94WnFEnOqp6H5VSEAGP1QzemeQ55Tj789flPvLAjeKOYC3U4yTI', serviceWorkerRegistration: registration });
-    })
-    .then((token) => {
-      console.log('FCM Token:', token);
-    })
-    .catch((err) => {
-      console.error('Failed to register Service Worker or get token:', err);
-    });
-} else {
-  console.warn('Service Worker is not supported in this browser.');
+// firebase-config.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-app.js";
+import { getMessaging, getToken, onMessage } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging.js";
+
+// Konfigurasi Firebase Anda
+const firebaseConfig = {
+  apiKey: "AIzaSyDo2kyDl39c4t5DfxYycmmjHSbY5FXB9AA",
+  authDomain: "sekawan-fc-427414.firebaseapp.com",
+  projectId: "sekawan-fc-427414",
+  storageBucket: "sekawan-fc-427414.appspot.com",
+  messagingSenderId: "399174955835",
+  appId: "1:399174955835:web:c681f8681c474420e8fd1e",
+  measurementId: "G-CD0MHD1RBP"
+};
+
+// Inisialisasi Firebase
+const app = initializeApp(firebaseConfig);
+
+// Inisialisasi Messaging
+const messaging = getMessaging(app);
+
+// Minta izin notifikasi dan dapatkan token FCM
+function requestNotificationPermission() {
+  Notification.requestPermission().then((permission) => {
+    if (permission === 'granted') {
+      console.log('Notification permission granted.');
+      return getToken(messaging, {
+        vapidKey: 'BPAqQpY9sfUZKGfJVpq6HKFoQp4THJ-ESMjE94WnFEnOqp6H5VSEAGP1QzemeQ55Tj789flPvLAjeKOYC3U4yTI'
+      });
+    } else {
+      console.error('Notification permission not granted.');
+      throw new Error('Notification permission not granted');
+    }
+  }).then((token) => {
+    console.log('FCM Token:', token);
+    // Kirim token ke server Anda di sini jika diperlukan
+  }).catch((err) => {
+    console.error('Unable to get permission to notify or get token:', err);
+  });
 }
+
+// Panggil fungsi ini ketika halaman dimuat
+requestNotificationPermission();
+
+// Menangani pesan saat aplikasi berada di latar depan
+onMessage(messaging, (payload) => {
+  console.log('Message received. ', payload);
+  // Tampilkan notifikasi atau perbarui UI sesuai kebutuhan
+});
