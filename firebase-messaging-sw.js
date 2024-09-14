fe64a4d2b89f7b2ee7f062b73af74f9576e2e508
Robithari <141445037+Robithari@users.noreply.github.com>
2024-09-14 08:46:23 +0700

    update

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,28 +1,28 @@
-importScripts('https://www.gstatic.com/firebasejs/9.1.2/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/9.1.2/firebase-messaging.js');
+importScripts('https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js');
 
-const firebaseConfig = {
-    apiKey: "AIzaSyDo2kyDl39c4t5DfxYycmmjHSbY5FXB9AA",
-    authDomain: "sekawan-fc-427414.firebaseapp.com",
-    projectId: "sekawan-fc-427414",
-    storageBucket: "sekawan-fc-427414.appspot.com",
-    messagingSenderId: "399174955835",
-    appId: "1:399174955835:web:c681f8681c474420e8fd1e",
-    measurementId: "G-CD0MHD1RBP"
-};
-
-// Inisialisasi Firebase
-firebase.initializeApp(firebaseConfig);
+// Firebase configuration
+firebase.initializeApp({
+  apiKey: "AIzaSyDo2kyDl39c4t5DfxYycmmjHSbY5FXB9AA",
+  authDomain: "sekawan-fc-427414.firebaseapp.com",
+  projectId: "sekawan-fc-427414",
+  storageBucket: "sekawan-fc-427414.appspot.com",
+  messagingSenderId: "399174955835",
+  appId: "1:399174955835:web:c681f8681c474420e8fd1e",
+  measurementId: "G-CD0MHD1RBP"
+});
 
 const messaging = firebase.messaging();
 
-messaging.onBackgroundMessage((payload) => {
-    console.log('Received background message ', payload);
-    const notificationTitle = 'Background Message Title';
-    const notificationOptions = {
-        body: 'Background Message body.',
-        icon: '/firebase-logo.png'
-    };
+// Handle background messages
+messaging.setBackgroundMessageHandler(function(payload) {
+  console.log('[firebase-messaging-sw.js] Received background message ', payload);
 
-    self.registration.showNotification(notificationTitle, notificationOptions);
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: '/icon.png'
+  };
+
+  return self.registration.showNotification(notificationTitle, notificationOptions);
 });
