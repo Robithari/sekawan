aa16818d2b043612382d44c8eaebd9c42108813f
Robithari <141445037+Robithari@users.noreply.github.com>
2024-10-19 22:19:53 +0700

    update

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const { initializeApp } = require('firebase/app');
 const { getFirestore, collection, query, where, getDocs } = require('firebase/firestore');
-
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -17,25 +16,26 @@ const firebaseConfig = {
   databaseURL: "https://sekawan-fc-427414-default-rtdb.firebaseio.com/"
 };
 
+// Inisialisasi Firebase dan Firestore
 const firebaseApp = initializeApp(firebaseConfig);
 const db = getFirestore(firebaseApp);
 
 // Middleware untuk file statis
 app.use(express.static('public'));
 
-// Rute dinamis untuk mengambil artikel berdasarkan slug
+// Route Dinamis: Ambil Data Artikel dan Sajikan Metadata OG di Server-Side
 app.get('/artikel/:slug', async (req, res) => {
   const { slug } = req.params;
 
   try {
-    // Query Firestore untuk mendapatkan artikel berdasarkan slug
+    // Query ke Firestore berdasarkan slug
     const q = query(collection(db, 'articles'), where('slug', '==', slug));
     const querySnapshot = await getDocs(q);
 
     if (!querySnapshot.empty) {
       const article = querySnapshot.docs[0].data();
 
-      // Sajikan HTML dengan metadata OG dinamis
+      // Sajikan HTML dengan Metadata OG Dinamis
       res.send(`
         <!DOCTYPE html>
         <html lang="en">
@@ -65,7 +65,7 @@ app.get('/artikel/:slug', async (req, res) => {
   }
 });
 
-// Jalankan server
+// Jalankan Server
 app.listen(PORT, () => {
   console.log(`Server berjalan di http://localhost:${PORT}`);
 });
