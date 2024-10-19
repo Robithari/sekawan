07ab05e37b08b90c0bc3156a1bc393b3369c2877
Robithari <141445037+Robithari@users.noreply.github.com>
2024-10-19 19:14:01 +0700

    update

--- server.js
+++ server.js
@@ -7,18 +7,10 @@ const { getFirestore, collection, getDocs, query, where } = require("firebase/fi
 // Inisialisasi aplikasi Express
 const app = express();
 
-// Konfigurasi Firebase menggunakan Environment Variables
+// Konfigurasi Firebase
 const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID,
-    measurementId: process.env.FIREBASE_MEASUREMENT_ID // Ini opsional
+    // Isi konfigurasi Firebase Anda di sini
 };
-
-// Inisialisasi Firebase
 initializeApp(firebaseConfig);
 const db = getFirestore();
 
@@ -65,9 +57,4 @@ app.get('/artikel-home.html', async (req, res) => {
 });
 
 // Mengekspor fungsi handler agar bisa digunakan di Vercel
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server berjalan di http://localhost:${PORT}`);
-});
-
 module.exports = app;
